3a3b096d66d2f4658240fce72a205bdd302e5e78
Orie Chinedu <oriechinedu@gmail.com>
2019-07-10 14:18:28 +0100

    Implement validatePostId middleware

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,5 +1,6 @@
-const express = 'express';
+const express = require('express');
 
+const Post = require('./postDb')
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -21,7 +22,18 @@ router.put('/:id', (req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+  const { id } = req.params;
+  if(!id || !Number(id)) {
+    next({statusCode: 400, message:"invalid post id" })
+  } else {
+    const post = await Post.getById(id);
+    if (post) {
+      req.post = post;
+      next()
+    } else {
+      next({statusCode: 400, message:"invalid post id"})
+    }
+  }
 };
 
 module.exports = router;
